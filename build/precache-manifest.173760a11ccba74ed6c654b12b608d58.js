36a80abf9065caf552ae780ded181eeeb5ae3284
Nithin <nithin@talville.com>
2019-05-15 18:38:45 +0530

    build

---
+++ build/precache-manifest.173760a11ccba74ed6c654b12b608d58.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a06a73666c6e8300238d56bb26bfd6c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c8befa2d239df3dd008d",
+    "url": "/static/css/main.2b412aa9.chunk.css"
+  },
+  {
+    "revision": "dd420427e40981369c01",
+    "url": "/static/js/2.a6a35eda.chunk.js"
+  },
+  {
+    "revision": "c8befa2d239df3dd008d",
+    "url": "/static/js/main.9417b77e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
